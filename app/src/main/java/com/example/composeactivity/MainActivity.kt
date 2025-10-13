c9b68593595718ab3b4adc33430b6f35b0c666f3
Harold_sp <harold.sanchez01@unicatolica.edu.co>
2025-10-13 18:36:58 -0500

    Completed guided example for section 3.03 (User Input)

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeactivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +28,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeActivityTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Activity3_03Screen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Activity3_03Screen(modifier: Modifier = Modifier) {
+    var textValue by remember { mutableStateOf("") }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = textValue,
+            onValueChange = { newText -> textValue = newText },
+            label = { Text("Escribe algo aquí") },
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Text(text = "Has escrito: $textValue")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Activity3_03Preview() {
     ComposeActivityTheme {
-        Greeting("Android")
+        Activity3_03Screen()
     }
 }
